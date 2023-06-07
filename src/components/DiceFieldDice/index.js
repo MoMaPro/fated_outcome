70836d6bdb0683636d71105a5ee2a6121907a752
ShinyStarFruit <moniquemonaspeight@gmail.com>
2023-06-07 18:34:23 -0400

    removes app.css styling and structures/styles dicestyle

--- src/components/DiceFieldDice/index.js
+++ src/components/DiceFieldDice/index.js
@@ -1,10 +1,13 @@
 import styled from "styled-components";
+import d6 from "./images/D6.png";
 //container that holds dice
 
 const DiceFieldDice = ({ dType, value, handleClick }) => {
   return (
     <DiceStyle>
-      <h3>{value}</h3>
+      <div>
+        <h3>{value}</h3>
+      </div>
     </DiceStyle>
   );
 };
@@ -12,5 +15,27 @@ const DiceFieldDice = ({ dType, value, handleClick }) => {
 export default DiceFieldDice;
 
 const DiceStyle = styled.div`
-  background-color: pink;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-image: url(${d6});
+  width: 7rem;
+  height: 7rem;
+
+  div {
+    font-size: 1.5rem;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    border-radius: 30px;
+    background-color: pink;
+    width: 3rem;
+    height: 3rem;
+  }
 `;
