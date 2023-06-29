0cf2d860d17b93d3e8d3265ca483fc9a7ce59ff3
Mark Trepanier <marktrepanier.cajigas@gmail.com>
2023-06-29 04:23:44 -0400

    adds task-02

--- src/components/Homework/tasks/index.js
+++ src/components/Homework/tasks/index.js
@@ -1,4 +1,5 @@
 import properties_example from "./task-00";
 import modify_state from "./task-01";
-const tasks = { properties_example, modify_state };
+import more_with_properties from "./task-02";
+const tasks = { properties_example, modify_state, more_with_properties };
 export default tasks;
---
+++ src/components/Homework/tasks/task-02.js
@@ -0,0 +1,21 @@
+//MORE WITH PROPERTIES
+//Task 2 solution is, instead of an Element, an object containing an Element and 'helper' function. Have the Element take in a function as a property and something else for that function to consume to then display in the element
+
+//Style to your hearts content
+const Solution = () => {
+  return <>your solution...</>;
+};
+
+//And Render Them
+const Render = () => {
+  return <Solution />;
+};
+
+//leave code below alone
+const solution = {
+  title: "more_with_properties",
+  summary:
+    "task-02: Create a helper function and then Create a react solution that would have a function as a property as well as something else be it a string, number or object. Have your custom function consume the other property in some way within your soultion",
+  item: <Render />,
+};
+export default solution;
