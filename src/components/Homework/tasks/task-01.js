f45242082e09ad9e92d2831c0236fddea3e8c3df
ShinyStarFruit <moniquemonaspeight@gmail.com>
2023-07-13 19:30:26 -0400

    Set up for useState and useEffect in task-01.  Renders record and includes styling.

--- src/components/Homework/tasks/task-01.js
+++ src/components/Homework/tasks/task-01.js
@@ -1,27 +1,68 @@
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
+import theme from "../../../styles/theme";
 
 //MODIFY STATE
 //The button displays what is in state. Finish setting up the UseState hook and set up the handler to modify state onClick
 //Style to your hearts content
-const Solution = () => {
-  const [state] = useState(1);
+const Solution = ({ record }) => {
+  const [clickCount, setClickCount] = useState(1);
+  const [timer, setTimer] = useState(60);
 
-  const handleClick = () => {};
+  const handleClick = () => {
+    setClickCount(clickCount + 1);
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTimer((prevTimer) => (prevTimer > 0 ? prevTimer - 1 : 0));
+    }, 1000);
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
 
   return (
-    <SolutionStyle onClick={handleClick}>
-      <strong>{state}</strong>
+    <SolutionStyle>
+      <div>
+        <h1>{timer} seconds</h1>
+        <h4>
+          My record for clicks in 1 minute is {record}. What's yours? *Drum
+          Roll* ...
+          <strong>{clickCount}</strong>
+        </h4>
+        <button className="button" onClick={handleClick}>
+          <strong>Click Me</strong>
+        </button>
+      </div>
     </SolutionStyle>
   );
 };
 
 //And Render Them
 const Render = () => {
-  return <Solution />;
+  return <Solution record="275" />;
 };
 
-const SolutionStyle = styled.button``;
+const SolutionStyle = styled.div`
+  h4,
+  h1 {
+    color: ${() => theme.primary};
+    background-color: ${() => theme.secondary};
+  }
+
+  .button {
+    background-color: ${() => theme.secondary};
+    color: ${() => theme.primary};
+    margin: 1em;
+    padding: 10px;
+    border-radius: 10px;
+  }
+  .button:hover {
+    color: ${() => theme.secondary};
+    background-color: ${() => theme.primary};
+  }
+`;
 
 //leave code below alone
 const solution = {
