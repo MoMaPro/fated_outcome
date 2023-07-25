2999d0578cfbbc103435452c69ee96b57b4687a4
ShinyStarFruit <moniquemonaspeight@gmail.com>
2023-07-25 16:13:17 -0400

    impliments the display trays as a background for the buttons

--- src/components/Inventory/InventoryItem.js
+++ src/components/Inventory/InventoryItem.js
@@ -1,7 +1,16 @@
+import { displayTray } from "./wares";
 const InventoryItem = ({ handleAdd, dType, productImage }) => {
   const handler = () => handleAdd(dType);
   return (
-    <button style={{ width: "100%", background: "none" }} onClick={handler}>
+    <button
+      style={{
+        width: "100%",
+        background: `url(${displayTray})`,
+        backgroundSize: "100%",
+        padding: 0,
+      }}
+      onClick={handler}
+    >
       <img src={productImage} alt={dType} width={"100%"} />
     </button>
   );
--- src/components/Inventory/wares/index.js
+++ src/components/Inventory/wares/index.js
@@ -5,6 +5,7 @@ import D10button from "./D10button.png";
 import D12button from "./D12button.png";
 import D20button from "./D20button.png";
 import D100button from "./D100button.png";
+import displayTray from "./displaytray.png";
 
 export {
   D4button,
@@ -14,4 +15,5 @@ export {
   D12button,
   D20button,
   D100button,
+  displayTray,
 };
