b769635de23ad333eb3b283d53df7871aac1de34
ShinyStarFruit <moniquemonaspeight@gmail.com>
2023-09-05 16:21:19 -0400

    Adds speechBubble image and SpeechStyle to Merchant file

--- src/components/Merchant/index.js
+++ src/components/Merchant/index.js
@@ -1,5 +1,26 @@
+import styled from "styled-components";
+import speechBubble from "./images/speechRectangle.png";
+
 const Merchant = ({ fatedOutcome }) => {
-  return <p>fated outcome:{` ${fatedOutcome}`}</p>;
+  return (
+    <SpeechStyle>
+      <img src={speechBubble} alt="speechBubble" width={"200px"} />
+      <p>fated outcome:{` ${fatedOutcome}`}</p>
+    </SpeechStyle>
+  );
 };
 
 export default Merchant;
+
+const SpeechStyle = styled.div`
+  display: inline-block;
+  position: relative;
+  img {
+    position: relative;
+  }
+  p {
+    position: absolute;
+    top: 10px;
+    left: 30px;
+  }
+`;
