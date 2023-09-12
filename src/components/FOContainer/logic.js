650a0afff8ee6c80b086116eecf32966e2f691d8
ShinyStarFruit <moniquemonaspeight@gmail.com>
2023-09-12 16:46:10 -0400

    Fixes currentQuote bug

--- src/components/FOContainer/logic.js
+++ src/components/FOContainer/logic.js
@@ -5,7 +5,7 @@ const DiceState = () => {
   const [modifier, setModifier] = useState(0);
   const [dice, setDice] = useState([]);
   const [total, setTotal] = useState();
-  const [currentQuote, setCurrentQuote] = useState("");
+  const [currentQuote, setCurrentQuote] = useState("Welcome to meow shop!");
 
   const handleModifier = (number) => {
     setModifier(number);
@@ -46,32 +46,7 @@ const DiceState = () => {
   };
 
   //Array of quotes/puns
-  const quotearray = [
-    "frrreel the meower of the dice!!!",
-    "Don't give up! Be purrsistent!",
-    "Your fate could be a cat-tastrophe or purr-fection!",
-    "With my guidance, your destiny's in good paws.",
-    "Dice in fish, paw on heart, roll the future I foresaw!",
-    "Fortune favors the feline! *wink*",
-    "Chaos everywhere MEOW! *throws dice in the air*",
-    "When Meowlin purrrrs the outcome stirs.",
-    "The dice don't lie, but I might keep secrets.",
-    "Making 'knead'-ed rolls since... well, always!",
-    "Dice-covery is my specialty. *yaaaawn*",
-    "Why did the bard bring a ladder to the dungeon? To reach the high 'notes'!",
-    "What's a vampire's favorite fruit? A 'blood' orange, of course!",
-    "Why did the rogue become a bard? He wanted to steal the show!",
-    "What do you call a cleric who tells jokes?  heal-larious!",
-    "The warlock's favorite snack? Hex-mex!",
-    "Squirrely business is nuts!",
-    "Magic 'beets' all expectations.",
-    "Owl you need is a little magic.",
-    "When a beholder needs glasses, it's a real eye-opener.",
-    "I tried to make a deal with a devil, but it was a real hell-ish negotiation!",
-    "What did the wizard say when he ran out of spell slots? I'm out of abracadabra!",
-    "When in doubt, polymorph it out!",
-    "When life gives you gelatinous cubes, make gelatinous cube-ade!",
-  ];
+  const quotearray = quotes;
 
   //shuffle function
   const shuffleArray = (array) => {
@@ -143,3 +118,30 @@ function randomizor(dType) {
 }
 
 export { randomizor, DiceState };
+
+const quotes = [
+  "frrreel the meower of the dice!!!",
+  "Don't give up! Be purrsistent!",
+  "Your fate could be a cat-tastrophe or purr-fection!",
+  "With my guidance, your destiny's in good paws.",
+  "Dice in fish, paw on heart, roll the future I foresaw!",
+  "Fortune favors the feline! *wink*",
+  "Chaos everywhere MEOW! *throws dice in the air*",
+  "When Meowlin purrrrs the outcome stirs.",
+  "The dice don't lie, but I might keep secrets.",
+  "Making 'knead'-ed rolls since... well, always!",
+  "Dice-covery is my specialty. *yaaaawn*",
+  "Why did the bard bring a ladder to the dungeon? To reach the high 'notes'!",
+  "What's a vampire's favorite fruit? A 'blood' orange, of course!",
+  "Why did the rogue become a bard? He wanted to steal the show!",
+  "What do you call a cleric who tells jokes?  heal-larious!",
+  "The warlock's favorite snack? Hex-mex!",
+  "Squirrely business is nuts!",
+  "Magic 'beets' all expectations.",
+  "Owl you need is a little magic.",
+  "When a beholder needs glasses, it's a real eye-opener.",
+  "I tried to make a deal with a devil, but it was a real hell-ish negotiation!",
+  "What did the wizard say when he ran out of spell slots? I'm out of abracadabra!",
+  "When in doubt, polymorph it out!",
+  "When life gives you gelatinous cubes, make gelatinous cube-ade!",
+];
